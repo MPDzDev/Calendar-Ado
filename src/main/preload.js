d3038b96e9b16c719be0d6d25ade359df8c8fa14
MPDzDev <139617967+MPDzDev@users.noreply.github.com>
2025-06-08 13:39:33 +0200

    Enable context isolation

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,7 +1,5 @@
-const { ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
-// With contextIsolation disabled we can assign directly to the window
-// object instead of using contextBridge.
-window.api = {
+contextBridge.exposeInMainWorld('api', {
   openWorkItems: (items) => ipcRenderer.send('open-work-items', items),
-};
+});
