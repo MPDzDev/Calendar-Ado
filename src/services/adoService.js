95705e26d6099558da2931fbb36c3f28ad9966ea
MPDzDev <139617967+MPDzDev@users.noreply.github.com>
2025-06-12 20:35:00 +0200

    Remove relations expansion from ADO work item request

--- src/services/adoService.js
+++ src/services/adoService.js
@@ -55,7 +55,7 @@ export default class AdoService {
   async _fetchItems(ids, auth) {
     const batch = ids.join(',');
     const res = await fetch(
-      `https://dev.azure.com/${this.org}/_apis/wit/workitems?ids=${batch}&fields=System.Id,System.Title,System.WorkItemType,System.Parent,System.TeamProject,System.Tags,System.AreaPath,System.IterationPath,System.State,Microsoft.VSTS.Scheduling.StoryPoints,Microsoft.VSTS.Common.AcceptanceCriteria&$expand=relations&api-version=7.0`,
+      `https://dev.azure.com/${this.org}/_apis/wit/workitems?ids=${batch}&fields=System.Id,System.Title,System.WorkItemType,System.Parent,System.TeamProject,System.Tags,System.AreaPath,System.IterationPath,System.State,Microsoft.VSTS.Scheduling.StoryPoints,Microsoft.VSTS.Common.AcceptanceCriteria&api-version=7.0`,
       {
         headers: { Authorization: auth },
       }
